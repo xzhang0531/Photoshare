3ec2c47276a86d08415f4afbe7d71b5fe530f8ed
Xiangyu Zhang <xzhang0531@gmail.com>
2018-08-10 17:13:38 -0400

    add side nav

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,7 @@ router.get("/login", function(req, res) {
 });
 
 router.post("/login", passport.authenticate("local", {
-		successRedirect: "secret",
+		successRedirect: "/",
 		failureRedirect: "login",
 	}), function(req, res) {
 });
